26fe27abddf425aa6f4c9aa9527c8622ace6c1aa
Vortext <762618186@qq.com>
2025-12-28 12:03:45 +0800

    Delete game_core directory

--- game_core/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GameCoreConfig(AppConfig):
-    name = "game_core"
--- game_core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Player
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        # 这里定义你想暴露给 Unity 的字段
-        # '__all__' 表示全部字段（id, username, level, gold...）
-        fields = '__all__'
